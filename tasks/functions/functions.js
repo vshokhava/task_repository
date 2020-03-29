e54519e639135032d420bf502a9e383715a698a2
Vasilisa Shokhava <vasilisa_shokhava@epam.com>
2020-03-29 19:27:16 +0300

    done hometask

--- tasks/functions/functions.js
+++ tasks/functions/functions.js
@@ -3,8 +3,9 @@
  *
  */
 function add(a, b) {
-
-}
+    return (a + b);
+};
+add (2, 8);
 
 /**
  * write function that return first and last name of given object
@@ -13,34 +14,61 @@ function add(a, b) {
  *    firstName: "John"
  *    lastName: "Dou"
  * }
+ *
  */
-function getFullName(object) {
-
-}
-
+/*var person = {
+    firstName: "John",
+    lastName : "Dou"};
+    */
+   function getFullName(firstName, lastName) {
+    var obj = {
+      firstName: firstName,
+      lastName: lastName
+  };
+    return(obj.firstName + " " + obj.lastName);
+  }
+  getFullName("John", "Dou");
 /**
  * write fuction that checks is number is odd
  * true if odd, false if even
  */
 function isOdd(n) {
-
-}
+    if (n%2 == 1) {
+  return true;
+    } else {
+    return false;
+    }
+};
+isOdd(2);
+isOdd(3);
+isOdd(0);
 
 /**
  * write function that return shortest of words in given array
  * e.g ["one", "two", "three"] should return one
  */
-function getShortest(wordArray) {
+var wordArray = ["one", "two", "three"];
 
-}
+function getShortest(arr){
+  return (
+  arr.reduce((a, b) => a.length <= b.length ? a : b)
+)
+};
+getShortest(wordArray);
 
-/**
- * write function that returns word google with given numbers of "o" symbols
+
+ /* write function that returns word google with given numbers of "o" symbols
  * e.g getGoogle(5) should return "gooooogle"
  */
+let letter = 'o'
 function getGoogle(n) {
+   return ('g' + letter.repeat(n) + 'gle');
 
-}
+};
+getGoogle(5)
+getGoogle(3)
+
+
 
 /**
  * write function that returns object based on given information (params may be null)
@@ -51,9 +79,26 @@ function getGoogle(n) {
  *    age: 42
  * }
  */
-function getUser(firstName, lastName, age) {
-
-}
+/*function getUser(firstName = null, lastName = null, age = null) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+  
+  var person = new getUser("John", "Doe", 42);
+  console.log (person);
+  var person = new getUser("John", "Doe");
+  console.log (person);
+*/
+function getUser(firstName = null, lastName = null, age = null) {
+  var obj = {
+    firstName: firstName,
+    lastName: lastName,
+    age: age
+};
+  return obj;
+};
+getUser("John", "Dou", 42);
 
 /**
  * write function that calculates total path traveled.
@@ -61,19 +106,21 @@ function getUser(firstName, lastName, age) {
  * e.g [{direction: "Kiyv - Minsk", distance: 567}, {direction: "Kiyv - Paris", distance: 2402}]
  */
 
-function getTotalPath(path) {
-
+const path = [
+    {  direction: "Kiyv - Minsk", distance: 567},
+    {  direction: "Kiyv - Paris", distance: 2402}
+];
+function getTotalPath(arr){
+      let total = 0;
+      let i;
+       for (i = 0; i < arr.length; i++) {
+              total += arr[i].distance;
+            }
+      return total;
 }
+getTotalPath(path);
 
-/**
- * write function that returns function that calculates given discount
- * e.g
- * const discount10 = discountFunction(10);
- * discount10(90) //returns 81
- */
-function discountFunction(discount) {
 
-}
 
 module.exports = {
     add,
@@ -83,5 +130,4 @@ module.exports = {
     getGoogle,
     getUser,
     getTotalPath,
-    discountFunction
 };
